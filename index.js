027c52eeac478a734b282eb6225ec0b88c28bfc2
MarkHelfinstine <mark.helfinstine@gmail.com>
2023-03-27 21:03:22 -0400

    added quote to img on Home view

--- index.js
+++ index.js
@@ -54,31 +54,45 @@ function afterRender(state) {
   }
 
   if (state.view === "Leaderboard") {
-    function sortTable() {
-      var table, rows, switching, i, x, y, shouldSwitch;
-      table = document.getElementById("stravaBoard");
-      switching = true;
-      while (switching) {
-        switching = false;
-        rows = table.rows;
-
-        for (i = 1; i < rows.length - 1; i++) {
-          shouldSwitch = false;
-
-          x = rows[i].getElementsByTagName("TD")[0];
-          y = rows[i + 1].getElementsByTagName("TD")[0];
-          if (x.innerHTML > y.innerHTML) {
-            shouldSwitch = true;
-            break;
-          }
-        }
-        if (shouldSwitch) {
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
-        }
+    function sortTableByColumn(table, column, asc = true) {
+      const dirModifier = asc ? 1 : -1;
+      const tBody = table.tBodies[0];
+      const rows = Array.from(tBody.querySelectorAll("tr"));
+      const sortedRows = rows.sort((a, b) => {
+        const aColText = a
+          .querySelector(`td:nth-child(${column + 1})`)
+          .textContent.trim();
+        const bColText = b
+          .querySelector(`td:nth-child(${column + 1})`)
+          .textContent.trim();
+        return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+      });
+      while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
       }
+      tBody.append(...sortedRows);
+      table
+        .querySelectorAll("th")
+        .forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+      table
+        .querySelector(`th:nth-child(${column + 1})`)
+        .classList.toggle("th-sort-asc", asc);
+      table
+        .querySelector(`th:nth-child(${column + 1})`)
+        .classList.toggle("th-sort-desc", !asc);
     }
-    sortTable();
+    document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+      headerCell.addEventListener("click", () => {
+        const tableElement =
+          headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(
+          headerCell.parentElement.children,
+          headerCell
+        );
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+      });
+    });
   }
 }
 
